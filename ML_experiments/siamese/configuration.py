ac2e5c5b7593c4bc1fb54c6ec2c75f503bc0761c
obu80 <45361348+obu80@users.noreply.github.com>
2022-11-02 11:55:18 +0300

    Update

--- ML_experiments/siamese/configuration.py
+++ ML_experiments/siamese/configuration.py
@@ -22,15 +22,15 @@ def str2bool(v):
 
 # Database and feature extraction parameters
 data_arg = parser.add_argument_group('Dataset')
-data_arg.add_argument('--audio_path_trn_per', type=str, default='../../data/feat/train', help="audio directory path for student train performances")
-data_arg.add_argument('--audio_path_test_per', type=str, default='../../data/feat/test', help="audio directory path for student test performances")
+data_arg.add_argument('--audio_path_trn_per', type=str, default='../../data/feat/train', help="audio directory path for student's train performances")
+data_arg.add_argument('--audio_path_test_per', type=str, default='../../data/feat/test', help="audio directory path for student's test performances")
 data_arg.add_argument('--audio_path_ref', type=str, default='../../data/feat/ref', help="audio directory path for references")
-data_arg.add_argument('--model_save_path', type=str, default='../../data/models', help="model directory path to save models")
-data_arg.add_argument('--w_size', type=float, default=0.025, help="window length (in sec)")
+data_arg.add_argument('--model_save_path', type=str, default='../../data/models', help="model directory path for saving models")
+data_arg.add_argument('--w_size', type=float, default=0.025, help="window size (in sec)")
 data_arg.add_argument('--s_size', type=float, default=0.025, help="skip size (in sec)")
 data_arg.add_argument('--max_frm', type=int, default=320, help="number of frames after padding (num_frame * skip_size = max duration in sec)")
-)data_arg.add_argument('--num_feat', type=int, default=20, help="dimension of the features") # 64 if the input is spectrogram
-data_arg.add_argument('--file_ext', type=str, default='_onset_CNN1D+LSTM.npy', help="extension for feature file in Siamese network")
+data_arg.add_argument('--num_feat', type=int, default=20, help="dimension of the features")
+data_arg.add_argument('--file_ext', type=str, default='_onset_CNN1D+LSTM.npy', help="extension for input files (Siamese network)")
 data_arg.add_argument('--lab_ext', type=str, default='none', help="extension for onset labels") # keep that 'none' in Siamese network, give the extension of onset labels when training the DNN-based onset detection
 
 # Model parameters
